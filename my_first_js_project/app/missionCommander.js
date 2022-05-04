77238937cf24f88e02cf30a8fd82a11d4651e2d6
Carolina G <ccarolina_mg@hotmail.com>
2022-05-04 00:11:15 -0500

    actualizando

---
+++ my_first_js_project/app/missionCommander.js
@@ -0,0 +1,10 @@
+class missionCommander{
+    constructor(name)
+    {
+        this.name = name
+    }
+}
+
+//esta linea nos permite exportar nuestra clase
+
+module.exports = missionCommander
---
+++ my_first_js_project/index.js
@@ -0,0 +1,14 @@
+const missionCommander = require('./app/missionCommander');
+const MissionCommander = require('./app/missionCommander');
+const kika = new MissionCommander("kika")
+const manchas = new MissionCommander("manchas")
+const sefia = new MissionCommander("sefia")
+const kiku = new MissionCommander("kiku")
+const kunkun = new MissionCommander("kunkun")
+
+
+console.log(kika.name)
+console.log(manchas.name)
+console.log(sefia.name)
+console.log(kiku.name)
+console.log(kunkun.name)
