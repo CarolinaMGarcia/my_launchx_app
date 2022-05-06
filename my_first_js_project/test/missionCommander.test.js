6523deea9e22b664c8b8b00b0058b6a8d7d3b06c
Carolina G <ccarolina_mg@hotmail.com>
2022-05-05 23:41:19 -0500

    actualizando ejercicio 1

--- my_first_js_project/test/missionCommander.test.js
+++ my_first_js_project/test/missionCommander.test.js
@@ -1,6 +1,15 @@
+//const missionCommander = require('./../app/missionCommander')
+
 describe("Esto es una suite de pruebas", () => {
     test('Caso de prueba 1', () => {
       const result = 1 + 2 
-      expect(result).toBe(10);
+      expect(result).toBe(3);
     });
   })
+
+/*describe("Unit Tests for Mission Commander Class", () => {
+    test('1) Create a mission commander object', () => {
+      const nombre = new missionCommander("kika")
+      expect(nombre.name).toBe("kika");
+    });
+  })*/
